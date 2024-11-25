cef9129f0a0d97a3f23f534a645514f1e0f2c0ba
Rishvaish <rishabpuranika.145@gmail.com>
2024-11-25 22:21:33 +0530

    changes with images and navigation rail names added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'dart:math';
+import 'package:passforge/savedata.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
         title: 'PassForge',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 0, 0)),
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 98, 0)),
         ),
         home: const MyHomePage(),
       ),
@@ -45,67 +47,194 @@ class MyAppState extends ChangeNotifier {
     );
     notifyListeners();
   }
+  /*
+  var passwords = <WordPair>[];
+  void addpasswords() {
+    if (passwords.contains(chars)) {
+      passwords.remove(chars);
+    } else {
+      passwords.add(current);
+    }
+    notifyListeners();
+  }*/
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = const PasswordGeneratorPage();
+        break;
+      case 1:
+        page = const Placeholder();
+        break;
+      case 2:
+        page = const Placeholder();
+        break;
+      case 3:
+        page = const Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+    
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  leading: Column(
+                    children: [
+                      // Custom image with fixed size
+                      Image.asset(
+                        'assets/logo.png', // Replace with your image path
+                        width: 60,
+                        height: 60,
+                      ),
+                      const SizedBox(height: 8),
+                      const Text(
+                        'PassForge', 
+                        style: TextStyle(
+                          fontSize: 20, 
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ],
+                  ),
+                  destinations: const [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.key_rounded),
+                      label: Text('Generator'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.account_balance_wallet_outlined),
+                      label: Text('Password Storage'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.manage_history),
+                      label: Text('History'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.settings),
+                      label: Text('Settings'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
+
+class PasswordGeneratorPage extends StatelessWidget {
+  const PasswordGeneratorPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('PassForge'),
-        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'Select the length of your password:',
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 16),
-              DropdownButton<int>(
-                value: appState.length,
-                items: [8, 12, 16, 20, 24].map<DropdownMenuItem<int>>((int value) {
-                  return DropdownMenuItem<int>(
-                    value: value,
-                    child: Text('$value characters'),
-                  );
-                }).toList(),
-                onChanged: (int? value) {
-                  if (value != null) {
-                    appState.setLength(value);
-                  }
-                },
-              ),
-              const SizedBox(height: 32),
-              const Text(
-                'Your password is:',
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 16),
-              SelectableText(
-                appState.password,
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.generateRandomPassword();
-                },
-                icon: const Icon(Icons.refresh),
-                label: const Text('Generate New Password'),
-              ),
-            ],
-          ),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Select the length of your password:',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 16),
+            DropdownButton<int>(
+              value: appState.length,
+              items: [8, 12, 16, 20, 24].map<DropdownMenuItem<int>>((int value) {
+                return DropdownMenuItem<int>(
+                  value: value,
+                  child: Text('$value characters'),
+                );
+              }).toList(),
+              onChanged: (int? value) {
+                if (value != null) {
+                  appState.setLength(value);
+                }
+              },
+            ),
+            const SizedBox(height: 32),
+            const Text(
+              'Your password is:',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 16),
+            SelectableText(
+              appState.password,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton.icon(
+              onPressed: () {
+                appState.generateRandomPassword();
+              },
+              icon: const Icon(Icons.refresh),
+              label: const Text('Generate New Password'),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+/*
+class display extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.passwords.isEmpty) {
+      return Center(
+        child: Text('No passwords yet.'),
+      );
+    }
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.passwords.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}*/
